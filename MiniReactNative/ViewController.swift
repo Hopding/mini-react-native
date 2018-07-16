141008942ccdcf795f07fac99f549fb463b9f597
Andrew Dillon <tajd1@unigroupinc.com>
2018-07-15 20:35:04 -0500

    Initial Commit

---
+++ MiniReactNative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniReactNative
+//
+//  Created by Dillon, Andrew on 7/15/18.
+//  Copyright © 2018 Dillon, Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
