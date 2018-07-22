2f42cd63315ceaea4d2eee59830593495db40dc9
Andrew Dillon <tajd1@unigroupinc.com>
2018-07-22 10:09:57 -0500

    Add root navigation controller

--- MiniReactNative/Renderer/ViewDescriptorRenderer.swift
+++ MiniReactNative/Renderer/ViewDescriptorRenderer.swift
@@ -12,8 +12,6 @@ class ViewDescriptorRenderer {
         rootView.subviews.forEach({ $0.removeFromSuperview() })
         
         for descriptor in toArray(viewDescriptors) {
-//            let view = createUIComponent(fromValue: descriptor)!
-//            rootView.addSubview(view)
             rootView.flex.addItem(createUIComponent(fromValue: descriptor)!)
         }
     }
--- MiniReactNative/main.js
+++ MiniReactNative/main.js
@@ -9,91 +9,86 @@ const colorMap = {
   green: 'purple',
 };
 
-// const makeButtons = (color1, color2) => [
-//   {
-//     type: 'View',
-//     x: 25,
-//     y: 25,
-//     width: 350,
-//     height: 600,
-//     color: 'black',
-//     children: [
-//       {
-//         type: 'Button',
-//         x: 100,
-//         y: 100,
-//         width: 100,
-//         height: 50,
-//         color: color1,
-//         title: 'JS Test 1',
-//         onPress: () => {
-//           log(`${color1} Button Pressed!`);
-//           renderButtons(colorMap[color1], colorMap[color2]);
-//         }
-//       },
-//       {
-//         type: 'Button',
-//         x: 100 + 100 + 25,
-//         y: 100,
-//         width: 100,
-//         height: 50,
-//         color: color2,
-//         title: 'JS Test 2',
-//         onPress: () => {
-//           log(`${color2} Button Pressed!`);
-//           renderButtons(colorMap[color1], colorMap[color2]);
-//         }
-//       }
-//     ]
-//   }
-// ];
-//
-// const renderButtons = (color1, color2) => render(makeButtons(color1, color2));
-//
-// renderButtons('red', 'purple');
-
-render([
+const makeButtons = (color1, color2) => [
   {
     type: 'View',
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     color: 'black',
     children: [
       {
-        type: 'View',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        // width: 1,
-        height: 1,
-        color: 'purple',
-        children: [
-          {
-            type: 'Button',
-            color: 'red',
-            width: 100,
-            height: 25,
-            // flex: 1,
-            title: 'Press Me!',
-            onPress: () => {
-              log('Tee hee, that tickles!');
-            },
-          },
-        ],
+        type: 'Button',
+        width: 100,
+        height: 50,
+        color: color1,
+        title: 'JS Test 1',
+        onPress: () => {
+          log(`${color1} Button Pressed!`);
+          renderButtons(colorMap[color1], colorMap[color2]);
+        },
       },
       {
-        type: 'View',
-        flex: 1,
-        // width: 1,
-        height: 1,
-        color: 'green',
-      },
-      {
-        type: 'View',
-        flex: 1,
-        // width: 1,
-        height: 1,
-        color: 'blue',
+        type: 'Button',
+        width: 100,
+        height: 50,
+        color: color2,
+        title: 'JS Test 2',
+        onPress: () => {
+          log(`${color2} Button Pressed!`);
+          renderButtons(colorMap[color1], colorMap[color2]);
+        },
       },
     ],
   },
-]);
+];
+
+const renderButtons = (color1, color2) => render(makeButtons(color1, color2));
+
+renderButtons('red', 'purple');
+
+// render([
+//   {
+//     type: 'View',
+//     flex: 1,
+//     color: 'black',
+//     children: [
+//       {
+//         type: 'View',
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         // width: 1,
+//         height: 1,
+//         color: 'purple',
+//         children: [
+//           {
+//             type: 'Button',
+//             color: 'red',
+//             width: 100,
+//             height: 25,
+//             // flex: 1,
+//             title: 'Press Me!',
+//             onPress: () => {
+//               log('Tee hee, that tickles!');
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         type: 'View',
+//         flex: 1,
+//         // width: 1,
+//         height: 1,
+//         color: 'green',
+//       },
+//       {
+//         type: 'View',
+//         flex: 1,
+//         // width: 1,
+//         height: 1,
+//         color: 'blue',
+//       },
+//     ],
+//   },
+// ]);
